77bf1b8289fd324db135f8243df3ec522bffda8f
Daniel Cárdenas <daniel_vsr@hotmail.com>
2022-04-16 15:27:25 -0300

    Cambios

--- enviar.php
+++ enviar.php
@@ -1,7 +1,7 @@
 <?php
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
-$empresa = $_POST['comentarios'];
+$mensaje = $_POST['mensaje'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'cardenas.daniel34@gmail.com';
+$para = 'daniel_vsr@hotmail.com';
 $asunto = 'Asistencia';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
